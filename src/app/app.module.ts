433bcb6aba4bf1899a86cdf336817756d2764c61
prparamesh <parameshwar507@gmail.com>
2022-06-05 11:46:35 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { RoutingModuleModule } from './routing-module/routing-module.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, RoutingModuleModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
---
+++ src/app/routing-module/routing-module.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+const routes: Routes = [{ path: 'test', component: WelcomeComponent }];
+@NgModule({
+  //imports: [CommonModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  declarations: [],
+})
+export class RoutingModuleModule {}
