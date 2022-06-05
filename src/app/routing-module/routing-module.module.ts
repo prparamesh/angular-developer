0597d01e8a66edc6435d8ddd503b6f4370b1674a
prparamesh <parameshwar507@gmail.com>
2022-06-05 12:29:23 +0530

    Routing

--- src/app/routing-module/routing-module.module.ts
+++ src/app/routing-module/routing-module.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { WelcomeComponent } from './welcome/welcome.component';
+import { WelcomeComponent } from '../welcome/welcome.component';
 const routes: Routes = [{ path: 'test', component: WelcomeComponent }];
 @NgModule({
   //imports: [CommonModule],
