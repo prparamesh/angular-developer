004323429de89729474fc84868f824b412305b86
prparamesh <parameshwar507@gmail.com>
2022-07-02 19:54:44 +0530

    2 files modified, 6 added - demonstrate different page navigation using Routes

--- src/app/routing-module/routing-module.module.ts
+++ src/app/routing-module/routing-module.module.ts
@@ -1,9 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from '../error/error.component';
+import { LoginComponent } from '../login/login.component';
 import { WelcomeComponent } from '../welcome/welcome.component';
 
-const routes: Routes = [{ path: 'test', component: WelcomeComponent }];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '**', component: ErrorComponent },
+];
 @NgModule({
   //imports: [CommonModule],
   imports: [RouterModule.forRoot(routes)],
